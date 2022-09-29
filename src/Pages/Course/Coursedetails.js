c31726d611067b30cccc223ca12c95918eb79520
Rakib-cloud <ri1641715@gmail.com>
2022-09-30 00:10:58 +0600

    course page work

--- src/Pages/Course/Coursedetails.js
+++ src/Pages/Course/Coursedetails.js
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 const Coursedetails = ({ tool }) => {
-    const {_id,courseName,instituitionName,courseDuration,courseDetails,coursePrice,image } = tool;
+    const {_id,courseName,institutionName,courseDuration,courseDetails,coursePrice,image } = tool;
     return (
         <div class="card lg:max-w-lg bg-base-100 shadow-xl">
         <div class="card-body text-center">
             <p class=" font-bold text-secondary">Course Name:{courseName}</p>
             <img src={image} alt="" />
-            <p class=" font-bold text-secondary">Instituion Name:{instituitionName}</p>
+            <p class=" font-bold text-secondary">Instituion Name:{institutionName}</p>
             <p class=" font-bold text-secondary">Course Duration:{courseDuration}</p>
             <p class=" font-bold text-secondary">Course Details :{courseDetails}</p>
             <p class=" font-bold text-secondary">price :{coursePrice}</p>
