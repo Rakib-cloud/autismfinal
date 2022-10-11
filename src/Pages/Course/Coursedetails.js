bd86520ce90e3d67c835520df0cd33ec05ba0f6f
YeasarArefin <yeasararefin007@gmail.com>
2022-10-12 01:42:51 +0600

    course purchase added

--- src/Pages/Course/Coursedetails.js
+++ src/Pages/Course/Coursedetails.js
@@ -1,21 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Coursedetails = ({ tool }) => {
-    const {_id,courseName,institutionName,courseDuration,courseDetails,coursePrice,image } = tool;
+    const { _id, courseName, institutionName, courseDuration, courseDetails, coursePrice, image, code } = tool;
     return (
         <div class="card lg:max-w-lg bg-base-100 shadow-xl">
-        <div class="card-body text-center">
-            <p class=" font-bold text-secondary">Course Name:{courseName}</p>
-            <img src={image} alt="" />
-            <p class=" font-bold text-secondary">Instituion Name:{institutionName}</p>
-            <p class=" font-bold text-secondary">Course Duration:{courseDuration}</p>
-            <p class=" font-bold text-secondary">Course Details :{courseDetails}</p>
-            <p class=" font-bold text-secondary">price :{coursePrice}</p>
-            <Link to={`/purchase/${_id}`} className="btn btn-accent" >Purchuse</Link>
-           
+            <div class="card-body text-center">
+                <p class=" font-bold text-secondary">Course Name:{courseName}</p>
+                <img src={image} alt="" />
+                <p class=" font-bold text-secondary">Instituion Name:{institutionName}</p>
+                <p class=" font-bold text-secondary">Course Duration:{courseDuration}</p>
+                <p class=" font-bold text-secondary">Course Details :{courseDetails}</p>
+                <p class=" font-bold text-secondary">price :{coursePrice}</p>
+                <p class=" font-bold text-secondary">code : {code}</p>
+                <Link to={`/purchase/${_id}`} className="btn btn-accent" >Purchuse</Link>
+
+            </div>
         </div>
-    </div>
     );
 };
 
--- src/Pages/Course/Purchasedetails.js
+++ src/Pages/Course/Purchasedetails.js
@@ -1,27 +1,42 @@
 import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useParams } from 'react-router-dom';
+import auth from '../../firebase.init';
 import Video from '../Videoplaylist/Video';
 
-
 const Purchasedetails = () => {
-     const { _id } = useParams();
-     
-  const [product, setProduct] = useState([]);
+  const { _id } = useParams();
 
-// akta api node js e create korte hobe 
+  const [user] = useAuthState(auth);
+  const [product, setProduct] = useState([]);
+  const [userInfo, setUserInfo] = useState({});
+  const [userCourses, setUserCourses] = useState([]);
+  console.log("🚀 ~ file: Purchasedetails.js ~ line 14 ~ Purchasedetails ~ userCourses", userCourses);
+  // console.log("🚀 ~ file: Purchasedetails.js ~ line 14 ~ Purchasedetails ~ userInfo", userInfo);
+
+  // akta api node js e create korte hobe 
   useEffect(() => {
     fetch(`https://autism60089s.herokuapp.com/course/${_id}`)
       .then(res => res.json())
       .then(data => setProduct(data));
-  }, [_id])
-  
-    return (
-        <div >
-            <div class="card bg-base-100 shadow-2xl w-1/2  items-center ml-64" >
-       
-                 
-                 <figure class="px-10 pt-10">
-          <img src={product.image } alt="" class="rounded-xl object-contain h-48 w-96" />
+  }, [_id]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/users/${user?.email}`)
+      .then(res => res.json())
+      .then(data => { setUserInfo(data); setUserCourses(data.courses); });
+  }, [user?.email]);
+
+  const purchased = userCourses.find(code => code === product.code);
+  console.log("🚀 ~ file: Purchasedetails.js ~ line 31 ~ Purchasedetails ~ purchased", purchased);
+
+  return (
+    <div >
+      <div class="card bg-base-100 shadow-2xl w-1/2  items-center ml-64" >
+
+
+        <figure class="px-10 pt-10">
+          <img src={product.image} alt="" class="rounded-xl object-contain h-48 w-96" />
         </figure>
         <div class="card-body items-center text-center">
           <h2 class="card-title">Course Name:{product.courseName}</h2>
@@ -29,16 +44,42 @@ const Purchasedetails = () => {
           <p>Course Duration:{product.courseDuration}</p>
           <p>Coursedetails:{product.courseDetails}</p>
           <p>course price:{product.coursePrice}</p>
+          <p>course code: <span className='text-red-600'>{product.code}</span></p>
+
+          {
+            purchased ? <><label htmlFor="my-modal-3" className="btn modal-button">open modal</label>
+              <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+              <div className="modal">
+                <div className="modal-box relative">
+                  <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                  <h3 className="text-lg font-bold text-green-500">You Have Successfully Purchased The Course</h3>
+                  <p className="py-4">Enjoy Your Content</p>
+                </div>
+              </div></> : <>
+              <label htmlFor="my-modal-3" className="btn modal-button">open modal</label>
+              <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+              <div className="modal">
+                <div className="modal-box relative">
+                  <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                  <h3 className="text-lg font-bold">Buy The Course</h3>
+                  <p className="py-4">Please Contact to the publisher with number +8801777777777</p>
+                </div>
+              </div>
+            </>
+          }
+
+
+
 
         </div>
-                 
-               
+
+
       </div>
-               <div>
-                <Video></Video>
-               </div>
-        </div>
-    );
+      <div>
+        <Video></Video>
+      </div>
+    </div>
+  );
 };
 
 export default Purchasedetails;
