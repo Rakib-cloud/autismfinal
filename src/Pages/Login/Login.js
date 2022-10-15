94c9bb1e44934908e8354d269984f86b18e840d8
YeasarArefin <yeasararefin007@gmail.com>
2022-10-15 18:33:12 +0600

    save user data in db

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,9 +1,10 @@
+import axios from 'axios';
 import React, { useEffect } from 'react';
 import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
-import auth from '../../firebase.init';
 import { useForm } from "react-hook-form";
+import auth from '../../firebase.init';
 
-import { Link, useNavigate, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import Loading from '../Shared/Loading/Loading';
 
 const Login = () => {
@@ -21,94 +22,141 @@ const Login = () => {
     const location = useLocation();
     let from = location.state?.from?.pathname || "/";
 
-    useEffect( () =>{
-        if (user || gUser) {
-            navigate(from, { replace: true });
+
+
+    useEffect(() => {
+
+        const saveData = (method, data) => {
+
+            fetch('http://localhost:5000/users', {
+                method,
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log("🚀 ~ file: Login.js ~ line 41 ~ useEffect ~ data", data);
+                    navigate(from, { replace: true });
+
+                });
+        };
+
+        // if (user) {
+        //     console.log("🚀 ~ file: Login.js ~ line 47 ~ useEffect ~ user", user);
+        //     axios.get(`http://localhost:5000/users/${user.user.email}`)
+        //         .then(res => {
+        //             console.log(res);
+        //             // if (!res.data.user.user.email) {
+        //             //     saveData('POST', data);
+        //             // }
+        //             // return;
+        //         });
+
+        // }
+
+        if (gUser) {
+            console.log("🚀 ~ file: Login.js ~ line 54 ~ useEffect ~ gUser", gUser.user.email);
+
+            axios.get(`http://localhost:5000/users/${gUser.user.email}`)
+                .then(res => {
+
+                    if (res.data.email === gUser.user.email) return;
+
+                    const data = {
+                        email: gUser.user.email,
+                        courses: []
+                    };
+                    saveData('PUT', data);
+                });
+
         }
-    }, [user, gUser, from, navigate])
+
+    }, [user, gUser, navigate, from]);
 
     if (loading || gLoading) {
-        return <Loading></Loading>
+        return <Loading></Loading>;
     }
 
-    if(error || gError){
-        signInError= <p className='text-red-500'><small>{error?.message || gError?.message }</small></p>
+    if (error || gError) {
+        signInError = <p className='text-red-500'><small>{error?.message || gError?.message}</small></p>;
     }
 
     const onSubmit = data => {
         signInWithEmailAndPassword(data.email, data.password);
-    }
+    };
 
     return (
         <div>
-            
+
             <div className='flex h-screen justify-center items-center'>
-            <div className="card w-96 bg-base-100 shadow-xl">
-                <div className="card-body">
-                    <h2 className="text-center text-2xl font-bold">Login</h2>
-                    <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="card w-96 bg-base-100 shadow-xl">
+                    <div className="card-body">
+                        <h2 className="text-center text-2xl font-bold">Login</h2>
+                        <form onSubmit={handleSubmit(onSubmit)}>
 
-                        <div className="form-control w-full max-w-xs">
-                            <label className="label">
-                                <span className="label-text">Email</span>
-                            </label>
-                            <input
-                                type="email"
-                                placeholder="Your Email"
-                                className="input input-bordered w-full max-w-xs"
-                                {...register("email", {
-                                    required: {
-                                        value: true,
-                                        message: 'Email is Required'
-                                    },
-                                    pattern: {
-                                        value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
-                                        message: 'Provide a valid Email'
-                                    }
-                                })}
-                            />
-                            <label className="label">
-                                {errors.email?.type === 'required' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
-                                {errors.email?.type === 'pattern' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
-                            </label>
-                        </div>
-                        <div className="form-control w-full max-w-xs">
-                            <label className="label">
-                                <span className="label-text">Password</span>
-                            </label>
-                            <input
-                                type="password"
-                                placeholder="Password"
-                                className="input input-bordered w-full max-w-xs"
-                                {...register("password", {
-                                    required: {
-                                        value: true,
-                                        message: 'Password is Required'
-                                    },
-                                    minLength: {
-                                        value: 6,
-                                        message: 'Must be 6 characters or longer'
-                                    }
-                                })}
-                            />
-                            <label className="label">
-                                {errors.password?.type === 'required' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
-                                {errors.password?.type === 'minLength' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
-                            </label>
-                        </div>
+                            <div className="form-control w-full max-w-xs">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input
+                                    type="email"
+                                    placeholder="Your Email"
+                                    className="input input-bordered w-full max-w-xs"
+                                    {...register("email", {
+                                        required: {
+                                            value: true,
+                                            message: 'Email is Required'
+                                        },
+                                        pattern: {
+                                            value: /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/,
+                                            message: 'Provide a valid Email'
+                                        }
+                                    })}
+                                />
+                                <label className="label">
+                                    {errors.email?.type === 'required' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                                    {errors.email?.type === 'pattern' && <span className="label-text-alt text-red-500">{errors.email.message}</span>}
+                                </label>
+                            </div>
+                            <div className="form-control w-full max-w-xs">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input
+                                    type="password"
+                                    placeholder="Password"
+                                    className="input input-bordered w-full max-w-xs"
+                                    {...register("password", {
+                                        required: {
+                                            value: true,
+                                            message: 'Password is Required'
+                                        },
+                                        minLength: {
+                                            value: 6,
+                                            message: 'Must be 6 characters or longer'
+                                        }
+                                    })}
+                                />
+                                <label className="label">
+                                    {errors.password?.type === 'required' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
+                                    {errors.password?.type === 'minLength' && <span className="label-text-alt text-red-500">{errors.password.message}</span>}
+                                </label>
+                            </div>
 
-                        {signInError}
-                        <input className='btn w-full max-w-xs text-white' type="submit" value="Login" />
-                    </form>
-                    <p><small>New to Autism care <Link className='text-primary' to="/signup">Create New Account</Link></small></p>
-                    <div className="divider">OR</div>
-                    <button
-                        onClick={() => signInWithGoogle()}
-                        className="btn btn-outline"
-                    >Continue with Google</button>
+                            {signInError}
+                            <input className='btn w-full max-w-xs text-white' type="submit" value="Login" />
+                        </form>
+                        <p><small>New to Autism care <Link className='text-primary' to="/signup">Create New Account</Link></small></p>
+                        <div className="divider">OR</div>
+                        <button
+                            onClick={() => signInWithGoogle()}
+                            className="btn btn-outline"
+                        >Continue with Google</button>
+                    </div>
                 </div>
-            </div>
-        </div >
+            </div >
         </div>
     );
 };
